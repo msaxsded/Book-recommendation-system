e11b6b548e094eade3702fa5361677b17c9ee3c9
msaxsded <153882928+msaxsded@users.noreply.github.com>
2023-12-19 14:41:27 +0800

    Update st.py

--- st.py
+++ st.py
@@ -1,1 +1,23 @@
-v
+import streamlit as st
+import pandas as pd
+
+# Load CSV files
+books = pd.read_csv('Books.csv')
+ratings = pd.read_csv('Ratings.csv')
+users = pd.read_csv('Users.csv')
+
+# Your recommendation system logic goes here...
+
+# Streamlit app layout
+st.title('Book Recommendation System')
+user_input = st.text_input('Enter User ID:', '1')
+
+# Display recommended books or personalized content based on the user input
+
+# Example: Displaying top-rated books
+top_rated_books = ratings.groupby('BookID')['Rating'].mean().sort_values(ascending=False).head(10)
+st.write('Top Rated Books:')
+st.write(books.loc[top_rated_books.index, 'BookTitle'])
+
+# Add more components and logic based on your recommendation system
+
