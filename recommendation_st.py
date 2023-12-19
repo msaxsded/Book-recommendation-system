f88e18661c748d8a6c21c6ac1bc11c74c76f85ed
msaxsded <153882928+msaxsded@users.noreply.github.com>
2023-12-19 15:08:57 +0800

    Update recommendation_st.py

--- recommendation_st.py
+++ recommendation_st.py
@@ -1,5 +1,6 @@
 import streamlit as st  
 import pandas as pd  
+import sklearn
 from sklearn.feature_extraction.text import CountVectorizer  
 from sklearn.metrics.pairwise import cosine_similarity  
   
