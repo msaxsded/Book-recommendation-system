114d9eba11f94a9e03d7fb5a3525804e5f5c7fff
msaxsded <153882928+msaxsded@users.noreply.github.com>
2023-12-19 14:15:16 +0800

    Update recommendation_st.py

--- recommendation_st.py
+++ recommendation_st.py
@@ -4,11 +4,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity  
   
 # 加载数据集  
-data = pd.read_csv('book_ratings.csv')  
+books_data = pd.read_csv('Books.csv')   # 包含书籍描述信息的文件  
+ratings_data = pd.read_csv('Ratings.csv')  # 包含用户评分信息的文件  
+users_data = pd.read_csv('Users.csv')   # 包含用户信息的文件  
   
-# 提取特征  
-book_features = data['book'].values  
-book_features = [f'{book} {features}' for book, features in zip(data['user'], book_features)]  
+# 提取特征：从书籍描述中提取特征  
+book_features = books_data['description'].values  # 假设书籍描述存储在'description'列中  
   
 # 创建CountVectorizer对象  
 vectorizer = CountVectorizer()  
@@ -22,21 +23,20 @@ similarity_matrix = cosine_similarity(book_vectors)
 # 构建推荐函数  
 def recommend(user):  
     # 获取用户已评分的书籍列表  
-    rated_books = data[data['user'] == user]['book'].values  
+    rated_books = ratings_data[ratings_data['user'] == user]['book'].values  
     # 获取用户未评分的书籍列表  
-    unrated_books = set(data['book'].unique()) - set(rated_books)  
+    unrated_books = set(books_data['book'].unique()) - set(rated_books)  
     # 获取与用户已评分书籍最相似的书籍列表  
     similar_books = [books[0] for books in similarity_matrix[user] if books[0] in unrated_books]  
-    # 返回推荐结果  
+    # 返回推荐结果，按相似度降序排列  
     return similar_books[::-1]  # 按相似度降序排列  
   
 # 展示推荐结果  
 st.write('Book Recommendation System')  
 st.write('===========================')  
 st.write('Enter a user ID to see their recommended books.')  
-st.text_input('User ID', '')  
-user = st.text_input('Enter a user ID:', '') or '1'  # 默认第一个用户为示例  
-recommended_books = recommend(user)  
-st.write('Recommended books for user:', user)  
+user_id = st.text_input('User ID', '') or '1'  # 默认第一个用户为示例  
+recommended_books = recommend(user_id)  # 将用户ID传递给推荐函数  
+st.write('Recommended books for user:', user_id)  
 st.write('===========================')  
-st.write(recommended_books)
+st.write(recommended_books) if recommended_books else st.write('No recommended books for this user.')
